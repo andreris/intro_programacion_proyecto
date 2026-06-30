9f4b1f734b1599033d8bbcd3437b8a6def4f4959
andreris <andre.rivera108@gmail.com>
2026-06-30 00:12:42 -0500

    numeros

--- script.js
+++ script.js
@@ -87,4 +87,72 @@ if (continuar.toLowerCase() !== "si") {
 
     alert("COMENZAMOS CON EL NIVEL 2: NUMEROS")
 
+// REINICIO DE PUNTAJES PARA EL NIVEL 2
+    for (let i = 0; i < puntajes.length; i++) {
+        puntajes[i] = 0
+    }
+
+    let ronda2 = 1
+
+// INDICACION DE CUANTAS RONDAS SERAN- ESTE CASO 7
+    while (ronda2 <= 7) {
+
+        alert("RONDA" + ronda2)
+
+        let turno2 = 0
+
+        while (turno2 < jugadores.length) {
+
+            let secuenciaNumeros = []
+// AUMENTO DE LA CANTIDAD DE NUMEROS PARA MEMORIZAR
+            while (secuenciaNumeros.length < ronda2) {
+                let aleatorio2 =
+                  Math.floor(Math.random() * 90) + 10
+
+                secuenciaNumeros.push(aleatorio2)
+            }
+//TURNO DEL JUGADOR
+            alert("Turno de " + jugadores[turno2] + "\nMemoriza:\n" +  secuenciaNumeros.join(" ")
+            )
+
+            let respuesta2 = prompt( "ESCRIBE LOS NUMEROS CON ESPACIOS:")
+
+            if (respuesta2 === secuenciaNumeros.join(" ")) {
+
+                alert("Atinaste: ganaste 10 puntos")
+//VALOR DE LOS PUNTAJES
+                puntajes[turno2] += 10
+
+            } else {
+
+                alert("Incorrecto.\nLa respuesta era:\n" + secuenciaNumeros.join(" "))
+            }
+
+            turno2++
+        }
+//MARCADOR DE PUNTAJES
+        let marcador2 = "PUNTAJES\n"
+
+        for (let i = 0; i < jugadores.length; i++) {
+
+            marcador2 +=
+                jugadores[i] + ": " + puntajes[i] + " puntos\n"
+        }
+
+        alert(marcador2)
+
+        ronda2++
+    }
+// SE DETERMINA EL GANADOR DEL NIVEL 2
+    let ganador2 = 0
+
+    for (let i = 1; i < puntajes.length; i++) {
+
+        if (puntajes[i] > puntajes[ganador2]) {
+            ganador2 = i
+        }
+    }
+//ANUNCIO DE GANADOR
+    alert( "GANADOR DEL NIVEL 2:" + jugadores[ganador2] + "\nPuntos:" + puntajes[ganador2])
+
 }
